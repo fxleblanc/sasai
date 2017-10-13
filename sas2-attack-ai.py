7f6a26f13c659a93e8740d64cf600912d784b46f
fxleblanc <felix.leblanc1305@gmail.com>
2017-10-13 18:58:09 -0400

    Save frames to video file

--- sas2-attack-ai.py
+++ sas2-attack-ai.py
@@ -1,4 +1,35 @@
 from pymouse import PyMouseEvent
+import pyscreenshot as ImageGrab
+import cv2
+import signal
+import numpy as np
+import sys
+
+def signal_handler(signal, frame):
+    cv2.destroyAllWindows()
+    sys.exit(0)
+
+def capture_images():
+    print("Capturing images")
+    frames = 50
+    count = 0
+    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    x = botright[0] - topleft[0]
+    y = botright[1] - topleft[1]
+    writer = cv2.VideoWriter('output.avi', fourcc, 20.0, (x, y))
+    if not writer.isOpened():
+        print("Video could not be opened")
+
+    while count <= frames:
+        img = ImageGrab.grab(bbox=(topleft[0], topleft[1], botright[0], botright[1]))
+        img_np = np.array(img)
+        frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2HSV)
+        print("Writing frame ", count)
+        writer.write(frame)
+        count += 1
+
+    writer.release()
+    sys.exit(0)
 
 class CaptureRectangle(PyMouseEvent):
     def __init__(self):
@@ -25,7 +56,9 @@ class CaptureRectangle(PyMouseEvent):
     def stop(self):
         print(topleft[0], topleft[1])
         print(botright[0], botright[1])
+        capture_images()
         PyMouseEvent.stop(self)
 
+signal.signal(signal.SIGINT, signal_handler)
 captureRectangle = CaptureRectangle()
 captureRectangle.run()
