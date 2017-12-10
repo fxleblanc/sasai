9a7145ab57355cdd323377bbb05bb203bdf16cd4
fxleblanc <felix.leblanc1305@gmail.com>
2017-12-09 21:19:33 -0500

    left bound of 325

--- attack_minigame.py
+++ attack_minigame.py
@@ -26,24 +26,26 @@ def start_actions_from_contours(cnts):
 
     # Bounds of contour capture
     right_bound = 500
+    left_bound = 325
+
     if cnts_len > 0:
         for contour in cnts:
             # Find coordinates and start threads
             circle = cv2.minEnclosingCircle(contour)
             cnt_pos_x = circle[0][0]
             cnt_pos_y = circle[0][1]
-            if cnt_pos_x <= right_bound and cnt_pos_x >= 325 and cnt_pos_y <= 180:
+            if cnt_pos_x <= right_bound and cnt_pos_x >= left_bound and cnt_pos_y <= 180:
                 action_thread = threading.Thread(name='thread_top',
                                                  target=top, args=(cnt_pos_x,))
                 action_thread.setDaemon(True)
                 action_thread.start()
-            elif (cnt_pos_x <= right_bound and cnt_pos_x >= 325 and
+            elif (cnt_pos_x <= right_bound and cnt_pos_x >= left_bound and
                   cnt_pos_y > 180 and cnt_pos_y < 260):
                 action_thread = threading.Thread(name='thread_right',
                                                  target=right, args=(cnt_pos_x,))
                 action_thread.setDaemon(True)
                 action_thread.start()
-            elif cnt_pos_x <= right_bound and cnt_pos_x >= 325 and cnt_pos_y >= 260:
+            elif cnt_pos_x <= right_bound and cnt_pos_x >= left_bound and cnt_pos_y >= 260:
                 action_thread = threading.Thread(name='thread_down',
                                                  target=down, args=(cnt_pos_x,))
                 action_thread.setDaemon(True)
